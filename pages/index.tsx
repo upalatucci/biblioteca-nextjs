ab16d3fee873ea69d910a6e978a6de9ff560572e
Ugo Palatucci <upalatuc@redhat.com>
2022-06-07 17:37:14 +0200

    carousel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ImageWithTextOverlay from "../components/ImageWithTextOverlay";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
 import HomeNavbar from "../components/Navbar/HomeNavbar";
 
 import raccoltaVol1 from "../public/raccolta-nichiren-vol1.jpeg";
@@ -12,9 +14,58 @@ import Link from "next/link";
 import SearchInput from "../components/SearchInput";
 import { useRouter } from "next/router";
 
+const responsive = {
+  0: { items: 1.3 },
+  700: { items: 2.3 },
+};
+
+const books = [
+  <ImageWithTextOverlay
+    image={raccoltaVol1}
+    title="Raccolta degli Scritti di Nichiren Daishonin"
+    description="Le lettere che il Daishonin inviava ai suoi discepoli"
+    width={475}
+    height={530}
+  />,
+
+  <ImageWithTextOverlay
+    image={raccoltaVol2}
+    title="Raccolta degli Scritti di Nichiren Daishonin"
+    description="Le lettere che il Daishonin inviava ai suoi discepoli"
+    width={475}
+    height={530}
+  />,
+
+  <ImageWithTextOverlay
+    image={sutraDelLoto}
+    title="Raccolta degli Scritti di Nichiren Daishonin"
+    description="Le lettere che il Daishonin inviava ai suoi discepoli"
+    width={475}
+    height={530}
+  />,
+];
+
+const presentationItems = [
+  <ImageWithTextOverlay
+    image={sfogliaGlossario}
+    title="Sfoglia il glossario"
+    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu ipsum urna."
+    width={800}
+    height={700}
+  />,
+  <ImageWithTextOverlay
+    width={800}
+    height={700}
+    image={ascolta}
+    title="Ascolta la Raccolta degli Scritti di Nichiren Daishonin"
+    description="Nam eu ipsum urna. Aenean odio nulla, mattis sit amet ipsum vel, commodo porttitor metus."
+  />,
+];
+
 export default function Home() {
   const router = useRouter();
   const [searchText, setSearchText] = useState("");
+  const [isMobile, setIsMobile] = useState(true);
 
   const onSearch = (e: React.FormEvent) => {
     e.preventDefault();
@@ -26,6 +77,18 @@ export default function Home() {
     });
   };
 
+  useEffect(() => {
+    const onResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", onResize);
+
+    onResize();
+
+    return () => window.removeEventListener("resize", onResize);
+  }, []);
+
   return (
     <>
       <Head>
@@ -35,48 +98,34 @@ export default function Home() {
       <div>
         <h1>NICHIREN Library</h1>
         <HomeNavbar />
-        <div>
-          <ImageWithTextOverlay
-            image={raccoltaVol1}
-            title="Raccolta degli Scritti di Nichiren Daishonin"
-            description="Le lettere che il Daishonin inviava ai suoi discepoli"
-            width={475}
-            height={530}
-          />
 
-          <ImageWithTextOverlay
-            image={raccoltaVol2}
-            title="Raccolta degli Scritti di Nichiren Daishonin"
-            description="Le lettere che il Daishonin inviava ai suoi discepoli"
-            width={475}
-            height={530}
+        {isMobile ? (
+          <AliceCarousel
+            mouseTracking
+            items={books}
+            responsive={responsive}
+            paddingLeft={30}
+            paddingRight={30}
+            disableButtonsControls
+            disableDotsControls
           />
+        ) : (
+          <section className="books">{books}</section>
+        )}
 
-          <ImageWithTextOverlay
-            image={sutraDelLoto}
-            title="Raccolta degli Scritti di Nichiren Daishonin"
-            description="Le lettere che il Daishonin inviava ai suoi discepoli"
-            width={475}
-            height={530}
+        {isMobile ? (
+          <AliceCarousel
+            mouseTracking
+            items={presentationItems}
+            responsive={responsive}
+            paddingLeft={30}
+            paddingRight={30}
+            disableButtonsControls
+            disableDotsControls
           />
-        </div>
-
-        <section className="books container">
-          <ImageWithTextOverlay
-            image={sfogliaGlossario}
-            title="Sfoglia il glossario"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu ipsum urna."
-            width={800}
-            height={700}
-          />
-          <ImageWithTextOverlay
-            width={800}
-            height={700}
-            image={ascolta}
-            title="Ascolta la Raccolta degli Scritti di Nichiren Daishonin"
-            description="Nam eu ipsum urna. Aenean odio nulla, mattis sit amet ipsum vel, commodo porttitor metus."
-          />
-        </section>
+        ) : (
+          <section className="books container">{presentationItems}</section>
+        )}
 
         <section className="section-search blank-section">
           <h2>Cosa vuoi approfondire oggi?</h2>
