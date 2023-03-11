697b6674f005a55c7433a88e515798a3af6cb7e7
Ugo Palatucci <upalatuc@redhat.com>
2023-03-11 19:06:14 +0100

    fix: new footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,115 +1,146 @@
 import * as React from "react";
+import Image from "next/image";
+
+import NRIcon from "@public/sites/nuovo-rinascimento.svg";
+import BSIcon from "@public/sites/buddismo-societa.svg";
+import IstitutoIcon from "@public/sites/istituto-buddista-italiano-soka-gakkai.svg";
+import EreditaIcon from "@public/sites/eredita-della-vita.svg";
+import OttoXMilleIcon from "@public/sites/otto-per-mille.svg";
+import VoloIcon from "@public/sites/volo-continuo.svg";
+import SenzatomicaIcon from "@public/sites/senzatomica.svg";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="relative bg-secondary text-white px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 py-12 lg:py-24 font-sans print:hidden">
-      <div className="flex flex-col md:flex-row">
-        <div className="w-full lg:w-4/6 lg:mx-4 lg:pr-8">
-          <h3 className="font-bold text-2xl">
-            Istituto buddista italiano soka gakkai
+    <footer className="relative bg-[#0D2444] text-white px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 py-12 lg:py-24 font-sans print:hidden">
+      <div className="flex flex-col md:flex-row justify-between">
+        <div className="lg:mx-4 lg:pr-8">
+          <h3 className="font-bold text-2xl text-white mb-10">
+            La Biblioteca di Nichiren
           </h3>
-          <p className="text-gray-400">
-            TUTTI I SITI DELL&apos;ISTITUTO BUDDISTA ITALIANO SOKA GAKKAI:
-          </p>
-        </div>
 
-        <div className="w-full lg:w-2/6 mt-8 lg:mt-0 lg:mx-4 lg:pr-8">
-          <ul className="mt-4">
+          <ul>
             <li>
               <a
-                href="https://ilnuovorinascimento.org"
+                href="https://www.sgi-italia.org/"
                 target="_blank"
-                className="flex items-center opacity-75 hover:opacity-100"
+                className="flex items-center opacity-75 hover:opacity-150"
                 rel="noreferrer"
               >
-                ilnuovorinascimento.org
+                Chi siamo
               </a>
             </li>
-            <li className="mt-4">
+            <li>
               <a
-                href="https://buddismoesocieta.org"
+                href="https://servizi.sgi-italia.org/abbonamenti/"
                 target="_blank"
-                className="flex items-center opacity-75 hover:opacity-100"
+                className="flex items-center opacity-75 hover:opacity-150"
                 rel="noreferrer"
               >
-                buddismoesocieta.org
-              </a>
-            </li>
-            <li className="mt-4">
-              <a
-                href="https://ilvolocontinuo.it"
-                target="_blank"
-                className="flex items-center opacity-75 hover:opacity-100"
-                rel="noreferrer"
-              >
-                ilvolocontinuo.it
-              </a>
-            </li>
-            <li className="mt-4">
-              <a
-                href="https://senzatomica.it"
-                target="_blank"
-                className="flex items-center opacity-75 hover:opacity-100"
-                rel="noreferrer"
-              >
-                senzatomica.it
+                Portale abbonamenti
               </a>
             </li>
           </ul>
         </div>
 
-        <div className="w-full lg:w-2/6 mt-8 lg:mt-0 lg:mx-4 lg:pr-8">
-          <ul className="mt-4">
-            <li>
-              <a
-                href="/rsnd-vol-1"
-                title=""
-                className="flex items-center opacity-75 hover:opacity-100"
-              >
-                Scritti di Nichiren Daishonin I
-              </a>
-            </li>
-            <li className="mt-4">
-              <a
-                href="/rsnd-vol-2"
-                title=""
-                className="flex items-center opacity-75 hover:opacity-100"
-              >
-                Scritti di Nichiren Daishonin II
-              </a>
-            </li>
-            <li className="mt-4">
-              <a
-                href="/"
-                title=""
-                className="flex items-center opacity-75 hover:opacity-100"
-              >
-                Sutra del Loto
-              </a>
-            </li>
-            <li className="mt-4">
-              <a
-                href="/glossario"
-                title=""
-                className="flex items-center opacity-75 hover:opacity-100"
-              >
-                Glossario
-              </a>
-            </li>
-            <li className="mt-4">
-              <a
-                href="/ricerca-avanzata"
-                title=""
-                className="flex items-center opacity-75 hover:opacity-100"
-              >
-                Ricerca avanzata
-              </a>
-            </li>
-          </ul>
-        </div>
+        <ul className="mt-4 flex flex-wrap max-w-[800px] items-center justify-center gap-4">
+          <li>
+            <a
+              href="https://www.sgi-italia.org/"
+              target="_blank"
+              className="flex items-center opacity-75 hover:opacity-150"
+              rel="noreferrer"
+            >
+              <Image width={150} height={100} src={IstitutoIcon} />
+            </a>
+          </li>
+          <li className="mt-4">
+            <a
+              href="https://senzatomica.it"
+              target="_blank"
+              className="flex items-center opacity-75 hover:opacity-150"
+              rel="noreferrer"
+            >
+              <Image width={150} height={100} src={SenzatomicaIcon} />
+            </a>
+          </li>
+          <li className="mt-4">
+            <a
+              href="https://www.ereditadellavita.it/"
+              target="_blank"
+              className="flex items-center opacity-75 hover:opacity-150"
+              rel="noreferrer"
+            >
+              <Image
+                width={150}
+                height={100}
+                src={EreditaIcon}
+                alt="Ereditá della vita"
+              />
+            </a>
+          </li>
+          <li className="mt-4">
+            <a
+              href="https://ottopermille.sokagakkai.it/"
+              target="_blank"
+              className="flex items-center opacity-75 hover:opacity-150"
+              rel="noreferrer"
+            >
+              <Image
+                width={150}
+                height={100}
+                src={OttoXMilleIcon}
+                alt="otto per mille"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://ilnuovorinascimento.org"
+              title=""
+              className="flex items-center opacity-75 hover:opacity-150"
+              rel="noreferrer"
+            >
+              <Image
+                width={180}
+                height={120}
+                src={NRIcon}
+                alt="nuovo rinascimento"
+              />
+            </a>
+          </li>
+          <li className="mt-4">
+            <a
+              href="https://buddismoesocieta.org"
+              title=""
+              className="flex items-center opacity-75 hover:opacity-150"
+              rel="noreferrer"
+            >
+              <Image
+                width={180}
+                height={120}
+                src={BSIcon}
+                alt="buddismo e societá"
+              />
+            </a>
+          </li>
+          <li className="mt-4">
+            <a
+              href="https://ilvolocontinuo.it"
+              className="flex items-center opacity-75 hover:opacity-150"
+              rel="noreferrer"
+            >
+              <Image
+                width={180}
+                height={120}
+                src={VoloIcon}
+                alt="volo continuo"
+              />
+            </a>
+          </li>
+        </ul>
       </div>
 
-      <p className="text-sm text-center text-gray-400 mt-12">
+      <p className="text-sm text-center text-gray-150 mt-12">
         © Istituto Buddista Italiano Soka Gakkai 2010-2021 | Via di Bellagio 2/E
         50141 Firenze FI | C.F.: 94069310483 | P.IVA: 04935120487 | privacy.
       </p>
