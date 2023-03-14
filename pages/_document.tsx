a79a0a158fec5b92a49a158a4b0ab9cc4d180230
Ugo Palatucci <upalatuc@redhat.com>
2023-03-14 17:32:04 +0100

    feat: pwa

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,8 +15,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
             rel="stylesheet"
           />
-          <meta name="Description" content="Libreria"></meta>
-          <meta property="og:description" content="Libreria"></meta>
 
           <meta
             name="robots"
@@ -27,6 +25,128 @@ class MyDocument extends Document {
             name="Keywords"
             content="Nichiren, Daishonin, Gosho, Sutra del Loto, Soka Gakkai, Soka, Gakkai, Buddismo, Buddha, Shakyamuni"
           ></meta>
+
+          <meta name="application-name" content="La Biblioteca di Nichiren" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta
+            name="apple-mobile-web-app-title"
+            content="La Biblioteca di Nichiren"
+          />
+          <meta
+            name="description"
+            content="Consulta e ricerca contenuti riguardanti Nichiren ed il Sutra del Loto in italiano"
+          />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta
+            name="msapplication-config"
+            content="/icons/browserconfig.xml"
+          />
+          <meta name="msapplication-TileColor" content="#2B5797" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="theme-color" content="#000000" />
+
+          <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/icons/touch-icon-ipad.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icons/touch-icon-iphone-retina.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="167x167"
+            href="/icons/touch-icon-ipad-retina.png"
+          />
+
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/icons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/icons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            rel="mask-icon"
+            href="/icons/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <link rel="shortcut icon" href="/favicon.ico" />
+
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:url" content="biblioteca.sgi-italia.org" />
+          <meta name="twitter:title" content="La Biblioteca di Nichiren" />
+          <meta
+            name="twitter:description"
+            content="Consulta e ricerca contenuti riguardanti Nichiren ed il Sutra del Loto in italiano"
+          />
+          <meta
+            name="twitter:image"
+            content="biblioteca.sgi-italia.org/icons/android-chrome-192x192.png"
+          />
+          <meta name="twitter:creator" content="@DavidWShadow" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="La Biblioteca di Nichiren" />
+          <meta
+            property="og:description"
+            content="Consulta e ricerca contenuti riguardanti Nichiren ed il Sutra del Loto in italiano"
+          />
+          <meta property="og:site_name" content="La Biblioteca di Nichiren" />
+          <meta property="og:url" content="biblioteca.sgi-italia.org" />
+          <meta
+            property="og:image"
+            content="biblioteca.sgi-italia.org/icons/apple-touch-icon.png"
+          />
+
+          <link
+            rel="apple-touch-startup-image"
+            href="/images/apple_splash_2048.png"
+            sizes="2048x2732"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            href="/images/apple_splash_1668.png"
+            sizes="1668x2224"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            href="/images/apple_splash_1536.png"
+            sizes="1536x2048"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            href="/images/apple_splash_1125.png"
+            sizes="1125x2436"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            href="/images/apple_splash_1242.png"
+            sizes="1242x2208"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            href="/images/apple_splash_750.png"
+            sizes="750x1334"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            href="/images/apple_splash_640.png"
+            sizes="640x1136"
+          />
         </Head>
         <body>
           <Main />
