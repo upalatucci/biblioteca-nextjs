a383684c4060d80d43c5caf28ef29872a4137423
valesilvetbp <valentina.silvestri@gmail.com>
2023-04-28 13:05:58 +0200

    fix footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Image from "next/image";
 
+import BnIcon from "@public/sites/biblioteca-nichiren.svg"
 import NRIcon from "@public/sites/nuovo-rinascimento.svg";
 import BSIcon from "@public/sites/buddismo-societa.svg";
 import IstitutoIcon from "@public/sites/istituto-buddista-italiano-soka-gakkai.svg";
@@ -8,22 +9,22 @@ import EreditaIcon from "@public/sites/eredita-della-vita.svg";
 import OttoXMilleIcon from "@public/sites/otto-per-mille.svg";
 import VoloIcon from "@public/sites/volo-continuo.svg";
 import SenzatomicaIcon from "@public/sites/senzatomica.svg";
+import Esperia from "@public/sites/esperia.svg";
 
 const Footer: React.FC = () => {
   return (
     <footer className="relative bg-[#0D2444] text-white px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 py-12 lg:py-24 font-sans print:hidden">
-      <div className="grid grid-cols-6 gap-2">
-        <div className="row-span-3 col-span-2">
+      <div className="grid grid-cols-2 md:grid-cols-6 gap-10 justify-items-center">
+        <div className="md:row-span-3 col-span-2 align-self-start md:justify-self-start">
           <a
-              href="https://www.sgi-italia.org/"
-              target="_blank"
-              className="flex items-center opacity-75 hover:opacity-150"
+              href="/"
+              className="flex items-center hover:opacity-75"
               rel="noreferrer"
             >
               <Image
                 width={190}
                 height={54}
-                src={IstitutoIcon}
+                src={BnIcon}
                 alt="La Biblioteca di Nichiren"
               />
           </a>
@@ -32,12 +33,12 @@ const Footer: React.FC = () => {
           <a
               href="https://www.sgi-italia.org/"
               target="_blank"
-              className="flex items-center opacity-75 hover:opacity-150"
+              className="flex items-center hover:opacity-75"
               rel="noreferrer"
             >
               <Image
-                width={150}
-                height={100}
+                width={188}
+                height={50}
                 src={IstitutoIcon}
                 alt="istituto buddista italiano soka gakkai"
               />
@@ -47,12 +48,12 @@ const Footer: React.FC = () => {
           <a
               href="https://senzatomica.it"
               target="_blank"
-              className="flex items-center opacity-75 hover:opacity-150"
+              className="flex items-center hover:opacity-75"
               rel="noreferrer"
             >
               <Image
-                width={150}
-                height={100}
+                width={130}
+                height={48}
                 src={SenzatomicaIcon}
                 alt="senzamotica"
               />
@@ -62,12 +63,12 @@ const Footer: React.FC = () => {
           <a
               href="https://www.ereditadellavita.it/"
               target="_blank"
-              className="flex items-center opacity-75 hover:opacity-150"
+              className="flex items-center hover:opacity-75"
               rel="noreferrer"
             >
               <Image
-                width={150}
-                height={100}
+                width={125}
+                height={47}
                 src={EreditaIcon}
                 alt="Ereditá della vita"
               />
@@ -77,12 +78,12 @@ const Footer: React.FC = () => {
           <a
               href="https://ottopermille.sokagakkai.it/"
               target="_blank"
-              className="flex items-center opacity-75 hover:opacity-150"
+              className="flex items-center hover:opacity-75"
               rel="noreferrer"
             >
               <Image
-                width={150}
-                height={100}
+                width={75}
+                height={65}
                 src={OttoXMilleIcon}
                 alt="otto per mille"
               />
@@ -90,14 +91,14 @@ const Footer: React.FC = () => {
         </div>
         <div>
           <a
+              target="_blank"
               href="https://ilnuovorinascimento.org"
-              title=""
-              className="flex items-center opacity-75 hover:opacity-150"
+              className="flex items-center hover:opacity-75"
               rel="noreferrer"
             >
               <Image
-                width={180}
-                height={120}
+                width={150}
+                height={26}
                 src={NRIcon}
                 alt="nuovo rinascimento"
               />
@@ -105,14 +106,14 @@ const Footer: React.FC = () => {
         </div>
         <div>
           <a
+              target="_blank"
               href="https://buddismoesocieta.org"
-              title=""
-              className="flex items-center opacity-75 hover:opacity-150"
+              className="flex items-center hover:opacity-75"
               rel="noreferrer"
             >
               <Image
-                width={180}
-                height={120}
+                width={190}
+                height={29}
                 src={BSIcon}
                 alt="buddismo e societá"
               />
@@ -120,13 +121,14 @@ const Footer: React.FC = () => {
         </div>
         <div>
           <a
+              target="_blank"
               href="https://ilvolocontinuo.it"
-              className="flex items-center opacity-75 hover:opacity-150"
+              className="flex items-center hover:opacity-75"
               rel="noreferrer"
             >
               <Image
-                width={180}
-                height={120}
+                width={168}
+                height={41}
                 src={VoloIcon}
                 alt="volo continuo"
               />
@@ -134,14 +136,15 @@ const Footer: React.FC = () => {
         </div>
         <div>
           <a
-              href="https://ilvolocontinuo.it"
-              className="flex items-center opacity-75 hover:opacity-150"
+              target="_blank"
+              href="http://esperiashop.it"
+              className="flex items-center hover:opacity-75"
               rel="noreferrer"
             >
               <Image
-                width={180}
-                height={120}
-                src={VoloIcon}
+                width={85}
+                height={28}
+                src={Esperia}
                 alt="esperia"
               />
             </a>
@@ -149,9 +152,9 @@ const Footer: React.FC = () => {
       </div>
       
 
-      <p className="text-sm text-center text-gray-150 mt-12">
+      <p className="text-sm text-gray-150 mt-12">
         © Istituto Buddista Italiano Soka Gakkai 2010-2021 | Via di Bellagio 2/E
-        50141 Firenze FI | C.F.: 94069310483 | P.IVA: 04935120487 | <a rel="nofollow" href="https://privacy.sgi-italia.org/">Privacy & Cookie Policy.</a>
+        50141 Firenze FI | C.F.: 94069310483 | P.IVA: 04935120487 | <a target="_blank" rel="nofollow noreferrer" href="https://privacy.sgi-italia.org/">Privacy & Cookie Policy.</a>
       </p>
     </footer>
   );
